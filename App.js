19bc560fe7a3cd14b1d41e5a5a80cfe21532dbe7
Archit Jain <arcyjain2002@gmail.com>
2022-01-13 19:58:54 +0530

    basic listing in the app

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}></View>;
+  const [Todo, setTodo] = useState([
+    { text: "work1", key: "1" },
+    { text: "work2", key: "2" },
+    { text: "work3", key: "2" },
+  ]);
+  return (
+    <View style={styles.container}>
+      {/* { header } */}
+      <View style={styles.content}>
+        {/* {content} */}
+        <View style={styles.list}>
+          <FlatList
+            data={Todo}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
